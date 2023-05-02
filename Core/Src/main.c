41e241b41185ee505cebbbffc39a1b41281afda6
maxime <maxdelalonde@gmail.com>
2023-05-02 11:48:57 +0200

    Majtestcode

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
+/** Jotaro
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
